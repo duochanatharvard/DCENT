3d6c62a1fd55886987b5239033ecf6a853c21cb6
duochanatharvard <chanduo.as@gmail.com>
2024-08-29 21:39:21 +0100

    add submit.sh for ERRSTR

---
+++ ERRSTR/submit.sh
@@ -0,0 +1,66 @@
+export reso=$1 
+export ERRSTR_mem=22150
+
+source ../DCENT_config.sh
+export dir_list_file="../dir_list.txt"
+dir_log=$(grep "log" "$dir_list_file" | head -n 1)
+echo $dir_log
+
+# Step 01 count_numbers  ###########################################################################
+export ERRSTR_S1=$(sbatch << EOF | egrep -o -e "\b[0-9]+$"
+#!/bin/sh
+#SBATCH --account=${cluster_account}
+#SBATCH -p ${cluster_use}
+#SBATCH -J ERRSTR_S1
+#SBATCH --nodes=1 
+#SBATCH --array=1-20
+#SBATCH -t ${cluster_time}
+#SBATCH --mem-per-cpu=${ERRSTR_mem}
+#SBATCH -o ${dir_log}/err_ERRSTR_S1
+
+matlab -nosplash -nodesktop -nodisplay -r "num=\${SLURM_ARRAY_TASK_ID}; addpath(genpath('..')); yr_list = (1849+num):20:2024; reso = ${reso}; ERRSTR_Step_01_count_numbers(yr_list,reso); quit;">>${dir_log}/log_ERRSTR_S1_\${SLURM_ARRAY_TASK_ID}
+
+EOF
+)
+
+echo Submitted Job ID : ERRSTR_S1 : ${ERRSTR_S1} 
+
+# Step 02 infer  ###########################################################################
+export ERRSTR_S2=$(sbatch << EOF | egrep -o -e "\b[0-9]+$"
+#!/bin/sh
+#SBATCH --account=${cluster_account}
+#SBATCH -p ${cluster_use}
+#SBATCH -J ERRSTR_S2
+#SBATCH --nodes=1 
+#SBATCH --array=1-20
+#SBATCH --dependency=afterok:${ERRSTR_S1}
+#SBATCH -t ${cluster_time}
+#SBATCH --mem-per-cpu=${ERRSTR_mem}
+#SBATCH -o ${dir_log}/err_ERRSTR_S2
+
+matlab -nosplash -nodesktop -nodisplay -r "num=\${SLURM_ARRAY_TASK_ID}; addpath(genpath('..')); yr_list = (1849+num):20:2024; reso = ${reso}; ERRSTR_Step_02_infer(yr_list,reso); quit;">>${dir_log}/log_ERRSTR_S2_\${SLURM_ARRAY_TASK_ID}
+
+EOF
+)
+
+echo Submitted Job ID : ERRSTR_S2 : ${ERRSTR_S2} 
+
+# Step 03 covariance matrix  ###########################################################################
+export ERRSTR_S3=$(sbatch << EOF | egrep -o -e "\b[0-9]+$"
+#!/bin/sh
+#SBATCH --account=${cluster_account}
+#SBATCH -p ${cluster_use}
+#SBATCH -J ERRSTR_S3
+#SBATCH --nodes=1 
+#SBATCH --array=1-20
+#SBATCH --dependency=afterok:${ERRSTR_S2}
+#SBATCH -t ${cluster_time}
+#SBATCH --mem-per-cpu=${ERRSTR_mem}
+#SBATCH -o ${dir_log}/err_ERRSTR_S3
+
+matlab -nosplash -nodesktop -nodisplay -r "num=\${SLURM_ARRAY_TASK_ID}; addpath(genpath('..')); yr_list = (1849+num):20:2024; reso = ${reso}; ERRSTR_Step_03_cov_matrix(yr_list,reso); quit;">>${dir_log}/log_ERRSTR_S3_\${SLURM_ARRAY_TASK_ID}
+
+EOF
+)
+
+echo Submitted Job ID : ERRSTR_S3 : ${ERRSTR_S3} 
