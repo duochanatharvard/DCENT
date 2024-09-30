2a3f27421982d4263c94e295e64a625d5f9b511b
duochanatharvard <chanduo.as@gmail.com>
2024-09-30 11:02:32 +0100

    calculate anomalies using overlapping period

--- SATH_V3/PHA/PHA_Functions/PHA_func_get_dif.m
+++ SATH_V3/PHA/PHA_Functions/PHA_func_get_dif.m
@@ -9,9 +9,16 @@ function [Dif_T, Tg_anm, Tn_anm] = PHA_func_get_dif(D,NET,ct_pair,Para)
     Tn        = D.T(NET(ct_pair,2),:);
 
     % Calculate anomalies using common coverage ---------------------------
-    Tg_anm    = CDC_demean(Tg,2,12);
-    Tn_anm    = CDC_demean(Tn,2,12);    
-    Dif_T     = Tg_anm - Tn_anm;
-
-    
+    do_overlap    = 1;
+    if do_overlap == 0
+        Tg_anm    = CDC_demean(Tg,2,12);
+        Tn_anm    = CDC_demean(Tn,2,12); 
+        Dif_T     = Tg_anm - Tn_anm;
+    else
+        if nargout > 1
+            Tg_anm    = CDC_demean(Tg,2,12);
+            Tn_anm    = CDC_demean(Tn,2,12); 
+        end
+        Dif_T     = CDC_demean(Tg - Tn,2,12);
+    end
 end
